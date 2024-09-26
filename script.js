23804774533449db2e15ceddfeb492f5c3967e64
JustinDub <justin.duban@hotmail.com>
2024-09-26 22:56:49 +0200

    manage rosters modal

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ rosterA.addEventListener("click", (e) => selectList(e));
 rosterB.addEventListener("click", (e) => selectList(e));
 
 function selectList(element) {
+	//Sélectionne une case d'un des rosters d'équipe
 	if(element.target.tagName=="IMG") {
 		if(element.target.parentElement.classList.contains("listPaired")) return;
 		if (selectedListId) unselectList();
@@ -43,6 +44,7 @@ function selectList(element) {
 }
 
 function unselectList() {
+	//Désélectionne une case d'un des rosters d'équipe
 	if(!selectedListId) return;
 	const selectedList = document.querySelector(`#${selectedListId}`);
 	selectedList.classList.remove("selected");
@@ -57,6 +59,7 @@ pairingA.addEventListener("click", (e) => attributePairing(e));
 pairingB.addEventListener("click", (e) => attributePairing(e));
 
 function attributePairing(element) {
+	//Modifie l'état d'une case du tableau de pairings
 	if(element.target.tagName=="IMG") {
 		if(
 			element.target.classList.contains("attacker") 
@@ -95,10 +98,58 @@ function attributePairing(element) {
 const eraserButton = document.querySelector("#eraser");
 eraserButton.addEventListener("click", (e) => unselectList());
 
+const modalButton = document.querySelector("#modal");
+modalButton.addEventListener("click", (e) => displayModal());
+const modalWrapper = document.querySelector(".modal-wrapper")
+
+function displayModal() {
+	//Affiche la modal de modification des rosters
+	const board = document.querySelector(".board")
+
+	modalWrapper.style.display = "flex"
+	let selectedFaction = {name: "", src: ""}
+
+	function selectFaction(factionElement) {
+		//Sélectionne une faction pour l'attribuer à un roster
+		if(factionElement.target.tagName=="IMG") {
+			if(selectedFaction.name) {
+				const prevFaction = document.querySelector(`.faction [title=${selectedFaction.name}]`)
+				prevFaction.parentElement.classList.remove("selected")
+			}
+			factionElement.target.parentElement.classList.add("selected")
+			selectedFaction.name = factionElement.target.title
+			selectedFaction.src = factionElement.target.src
+			const modal = document.querySelector(".modal")
+			modal.addEventListener("click", (rosterElement) => {
+				if(rosterElement.target.tagName=="IMG" && rosterElement.target.parentElement.classList.contains("list")) {
+					//Met à jour la liste sélectionnée dans le roster
+					rosterElement.target.src = factionElement.target.src
+					const listElement = board.querySelector(`.roster #${rosterElement.target.parentElement.id} img`)
+					listElement.src = factionElement.target.src
+				}
+			});
+		}
+	}
+	const factionsRoster = document.querySelector(".factionsRoster")
+	factionsRoster.addEventListener("click", selectFaction)
+
+	function validateRoster(event) {
+		//ferme la modal de modification des rosters
+		const prevFaction = document.querySelector(`.faction [title=${selectedFaction.name}]`)
+		prevFaction.parentElement.classList.remove("selected")
+		modalWrapper.style.display = "none"
+        document.removeEventListener("click", validateRoster);
+        document.removeEventListener("click", selectFaction);
+	}
+	const validateButton = document.querySelector("#validateRoster");
+	validateButton.addEventListener("click", validateRoster);
+}
+
 const clearButton = document.querySelector("#clearAll");
 clearButton.addEventListener("click", (e) => clearAll());
 
 function clearAll() {
+	//Réinitialise la page
 	const pairedList = document.querySelectorAll(".paired img");
 	pairedList.forEach((pairedElement) => {
 		const className = pairedElement.className;
